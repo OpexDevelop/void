278f22b130ba0b2657dcc8351efe9c6ac247b1ed
Opex <71468821+OpexDevelop@users.noreply.github.com>
2026-04-14 23:26:17 +0300

    fix: fix очепятку

--- flutter_app/lib/screens/setup_screen.dart
+++ flutter_app/lib/screens/setup_screen.dart
@@ -1,4 +1,4 @@
-limport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../state/app_state.dart';
 import '../state/chat_state.dart';
