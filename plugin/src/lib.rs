264afaf5de8f56f53a1cd1b89581d54d99e06ad1
OpexDevelop <orehorehov6@gmail.com>
2026-04-18 21:29:32 +0300

    fix android

--- plugin/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use extism_pdk::*;
-
-// Этот макрос автоматически делает функцию доступной для Хоста
-#[plugin_fn]
-pub fn greet(name: String) -> FnResult<String> {
-    let result = format!("Привет, {}! Я работаю изнутри Wasm, и я не сломал память!", name);
-    Ok(result)
-}
